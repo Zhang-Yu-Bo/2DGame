2f34df620c13246a4839f69a5ef6d5c855991532
mosquitoYee <kenwen1999@gmail.com>
2020-04-26 01:33:05 +0800

    [Update]PlayerFall & AreaEffector

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform target;
+	Transform _Trans;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		_Trans = GetComponent<Transform>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 followPos = new Vector3(target.position.x, target.position.y, _Trans.position.z);
+		if (followPos.x < 0)
+			followPos.x = 0;
+		else if (followPos.x > 50)
+			followPos.x = 50;
+		if (followPos.y < -27)
+			followPos.y = -27;
+		else if (followPos.y > 10)
+			followPos.y = 10;
+		//Debug.Log(followPos);
+		_Trans.position = followPos;
+	}
+}
---
+++ Assets/Scripts/DownEdge.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownEdge : MonoBehaviour
+{
+	public Rigidbody2D player;
+	Rigidbody2D downEdge;
+	Vector2 beginPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		downEdge = GetComponent<Rigidbody2D>();
+		beginPos = new Vector2(player.position.x, player.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log(player.position);
+		player.transform.position = beginPos;
+	}
+}
