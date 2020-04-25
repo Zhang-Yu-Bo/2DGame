e508f7a7739f01fe7f5af3e54ed92b47101a855d
mosquitoYee <kenwen1999@gmail.com>
2020-04-25 16:27:18 +0800

    [Update]Import Map Objects

---
+++ Assets/Scripts/CameraTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTest : MonoBehaviour
+{
+	Transform _Trans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		_Trans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			_Trans.Translate(new Vector2(10 * Time.deltaTime, 0));
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow)) 
+		{
+			_Trans.Translate(new Vector2(-10 * Time.deltaTime, 0));
+		}
+    }
+}
