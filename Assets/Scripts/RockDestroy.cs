7815cee126b64df82c25cb461e984bc12c1e429b
mosquitoYee <kenwen1999@gmail.com>
2020-04-26 17:34:24 +0800

    [Update]Freeze Shader

---
+++ Assets/Scripts/RockDestroy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockDestroy : MonoBehaviour
+{
+	public int attackCount;
+	public Material m_Material;
+	Shader freeze, normal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		attackCount = 0;
+		freeze = Shader.Find("Custom/FreezeRock");
+		normal = m_Material.shader;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Freeze Ball")
+		{
+			attackCount++;
+			if (attackCount == 1)
+				m_Material.shader = freeze;
+			else if (attackCount == 2)
+			{
+				m_Material.shader = normal;
+				Destroy(this.gameObject);
+			}
+		}
+	}
+}
