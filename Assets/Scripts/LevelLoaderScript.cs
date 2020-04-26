bf96d6a63fdc0fd7b1a7af9c0dde3c0d3e04f699
Zhang-Yu-Bo <46323902+Zhang-Yu-Bo@users.noreply.github.com>
2020-04-26 16:14:47 +0800

    [Update] Change scene

---
+++ Assets/Scripts/LevelLoaderScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoaderScript : MonoBehaviour
+{
+
+    public Animator animator;
+    public float transitionTime = 1.0f;
+    public Canvas canvas;
+
+    public void LoadScene(int index)
+    {
+        StartCoroutine(this.LoadLevel(index));
+    }
+
+    IEnumerator LoadLevel(int index)
+    {
+        animator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(this.transitionTime);
+
+        SceneManager.LoadScene(index);
+    }
+
+    public void setEnable(int flag)
+    {
+        /*
+        if (flag == 0)
+        {
+            this.canvas.enabled = false;
+        }
+        else
+        {
+            this.canvas.enabled = true;
+        }
+        */
+        Debug.Log(1);
+    }
+}
