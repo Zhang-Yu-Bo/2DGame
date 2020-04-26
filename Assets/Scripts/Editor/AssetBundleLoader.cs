adcd9ec2c980d6b3fe8699c6600ea2a3d40ab106
Zhang-Yu-Bo <46323902+Zhang-Yu-Bo@users.noreply.github.com>
2020-04-26 20:03:56 +0800

    [Update] AssetBundles (Not Complete)

---
+++ Assets/Scripts/Editor/AssetBundleLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+public class AssetBundleLoader
+{
+    [MenuItem("Assets/AssetBundle Builders/Build All AssetBundles (Windows x64)")]
+    static void CreateAssetBundles_win_x64()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles",
+            BuildAssetBundleOptions.None,
+            BuildTarget.StandaloneWindows64);
+    }
+}
